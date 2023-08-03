e9ecc7402915f3dc80aa878b63b182826e3c17ed
qingwen123 <15219913558@163.com>
2023-08-03 22:00:13 +0800

    8.3

---
+++ Inrared/.Inrared.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ykl/linux/drive/linux/Inrared/Inrared.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/ykl/linux/drive/linux/Inrared/Inrared.ko /home/ykl/linux/drive/linux/Inrared/Inrared.o /home/ykl/linux/drive/linux/Inrared/Inrared.mod.o
---
+++ Inrared/.tmp_versions/Inrared.mod
@@ -0,0 +1,2 @@
+/home/ykl/linux/drive/linux/Inrared/Inrared.ko
+/home/ykl/linux/drive/linux/Inrared/Inrared.o
